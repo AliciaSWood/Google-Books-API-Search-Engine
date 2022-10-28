e9f66a7fd36f62f2af5d4797d68b5b76af75434b
Alicia W <aliciaw_silvie@outlook.com>
2022-10-28 15:24:42 +1030

    more-changes

--- google-books-api/src/containers/BookLibrary/BookLibrary.jsx
+++ google-books-api/src/containers/BookLibrary/BookLibrary.jsx
@@ -18,16 +18,30 @@ const bookInfo = {
     description: "",
     imageLinks: ""
 }
+
+
 if(book.authors== undefined){
-    bookInfo.authors = "No author"
-}else bookInfo.authors = book.authors[0]
+    bookInfo.authors = "No Author Provided"}
+    else if(book.authors.length <= 2) {
+        bookInfo.authors = book.authors.join(' and ');
+      } else if (book.authors.length > 2) {
+        let lastAuthor = ' and ' + book.authors.slice(-1);
+        book.authors.pop();
+        bookInfo.authors = book.authors.join(', ');
+        bookInfo.authors += lastAuthor;
+      }
+else bookInfo.authors = book.authors[0]
 
 if(book.description && book.description.length >= 50){
-    bookInfo.description = book.description.slice(0, 50)
+    bookInfo.description = book.description.slice(0, 50) + "..."
 } else bookInfo.description = book.description 
 
+if(book.title && book.title.length >=50){
+    bookInfo.title = book.title.slice(0, 50) + "..."
+}else bookInfo.title = book.title
+
 if(book.imageLinks == undefined){
-    bookInfo.imageLinks = "BackUp Image/Missing"
+    bookInfo.imageLinks = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
 } else bookInfo.imageLinks = book.imageLinks.thumbnail
 
                     return (<BookCard bookInfo={bookInfo}/>)
